0fe6935c0fc1138be4581b702726c883e01fe61d
abhi614s <abhinandan.s.brudite@gmail.com>
2024-01-05 21:55:15 +0530

    Solved Advance/QA6.py

--- Advance/QA6.py
+++ Advance/QA6.py
@@ -12,11 +12,16 @@ class C(B):
     def func2(self):
         print("func2 of B")
 
+class D(A, B, C):
+    def fun3(self):
+        print('fun3 of B')
 
 a = A()
 b = B()
 c = C()
+c = D()
 
 a.display()
 b.display()
 c.display()
+d.display()
