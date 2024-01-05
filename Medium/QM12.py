7682b194cb072441a8ec4855bf3ed70a45d465f7
abhi614s <abhinandan.s.brudite@gmail.com>
2024-01-05 21:04:00 +0530

    resolved Medium/QM12.py

--- Medium/QM12.py
+++ Medium/QM12.py
@@ -1,13 +1,24 @@
 # Create a login page backend to ask users to enter the username and password. Make sure to ask for a Re-Type Password and if the password is incorrect give chance to enter it again but it should not be more than 3 times.
 
-password = 'h2o'
+password = 'pwd'
 chance = 3
+
 str1 = input('Enter you Password: ')
 
 while chance:
     if str1 == password:
         print('Welcome')
         break
+    
     else:
-        str1 = input('Re Enter you Password: ')
-        chance -= 1
+        print('Wrong Password')
+        print(f"{chance} attempts left")
+        
+        if chance:
+            str1 = input('Re Enter you Password: ')
+            chance -= 1
+            
+            if str1 == password:
+                print(':::: Welcome ::::')
+                break
+    
